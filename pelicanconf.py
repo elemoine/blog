b269a169c0c7f35e81fadaf3ebf002b8a3ed281b
Éric Lemoine <eric.lemoine@gmail.com>
2012-12-29 22:09:15 +0100

    Minor edits to pelican conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,20 +3,20 @@
 
 AUTHOR = u'\xc9ric Lemoine'
 SITENAME = u'\xc9ric Lemoine'
-SITEURL = ''
+SITEURL = 'http://erilem.net'
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          #('Python.org', 'http://python.org'),
+          #('Jinja2', 'http://jinja.pocoo.org'),
+          #('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'http://twitter.com/erilem'),
+          ('github', 'http://github.com/elemoine'))
 
 DEFAULT_PAGINATION = 10
