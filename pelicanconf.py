7606a5b86f688c328ff4559950457e5b4f00cf40
Éric Lemoine <eric.lemoine@gmail.com>
2012-12-30 22:29:39 +0100

    Use alphabetic order in conf file

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,21 +2,15 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = u'\xc9ric Lemoine'
-SITENAME = u'\xc9ric Lemoine'
-SITEURL = 'http://erilem.net'
-
-TIMEZONE = 'Europe/Paris'
-
 DEFAULT_LANG = u'en'
-
-# Blogroll
+DEFAULT_PAGINATION = 10
+DISPLAY_PAGES_ON_MENU = True
 #LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
           #('Python.org', 'http://python.org'),
           #('Jinja2', 'http://jinja.pocoo.org'),
           #('You can modify those links in your config file', '#'),)
-
-# Social widget
+SITENAME = u'\xc9ric Lemoine'
+SITEURL = 'http://erilem.net'
 SOCIAL = (('twitter', 'http://twitter.com/erilem'),
           ('github', 'http://github.com/elemoine'))
-
-DEFAULT_PAGINATION = 10
+TIMEZONE = 'Europe/Paris'
