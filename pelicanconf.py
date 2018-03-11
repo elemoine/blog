4f084c0c5beb5d40bf0fddc021bde8eb34289176
Éric Lemoine <eric.lemoine@oslandia.com>
2018-03-11 21:51:20 +0100

    Update pelican conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
 
-AUTHOR = u'\xc9ric Lemoine'
-ARTICLE_EXCLUDES = (('pages', 'cv'))
-DEFAULT_LANG = u'en'
-DEFAULT_CATEGORY = 'Misc'
-DEFAULT_DATE = u'fs'
-DEFAULT_PAGINATION = 10
-DISQUS_SITENAME = 'erilem'
-FILES_TO_COPY = (('CNAME', 'CNAME'),
-                 ('cv/cv.en.html', 'cv/cv.en.html'),
-                 ('cv/cv.css', 'cv/cv.css'),
-                 ('cv/cv.en.pdf', 'cv/cv.en.pdf'))
-#LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          #('Python.org', 'http://python.org'),
-          #('Jinja2', 'http://jinja.pocoo.org'),
-          #('You can modify those links in your config file', '#'),)
-SITENAME = u'\xc9ric Lemoine'
+AUTHOR = 'Éric Lemoine'
+SITENAME = 'Éric Lemoine'
 SITEURL = 'http://erilem.net'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Social widget
 SOCIAL = (('Twitter', 'http://twitter.com/erilem'),
           ('GitHub', 'http://github.com/elemoine'),
           ('Google+', 'https://plus.google.com/u/1/101823398102994275785'),
           ('Linkedin', 'http://www.linkedin.com/in/erilem'))
-TIMEZONE = 'Europe/Paris'
+
+DEFAULT_PAGINATION = 10
+
+ARTICLE_EXCLUDES = ['cv']
+STATIC_PATHS = ['cv/cv.en.html',
+                'cv/cv.en.pdf',
+                'cv/cv.css',
+                'CNAME',
+                'keybase.txt',
+                'eric-lemoine-openpgp4fpr.png']
+DEFAULT_CATEGORY = 'Blog'
+MENUITEMS = [('Resume', '/cv/cv.en.html')]
+
+RELATIVE_URLS = True
